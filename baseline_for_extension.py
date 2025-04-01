c7e09a0208d77442c440439e5612e498a5b1f280
candidate1089687 <candidate1089687@gmail.com>
2025-04-01 16:48:27 +0100

    Add baseline method for extension

--- baseline_for_extension.py
+++ baseline_for_extension.py
@@ -1,8 +1,8 @@
 from itertools import chain
 import wandb
 import torch
-from torch.nn import KLDivLoss
-from torcheval.metrics.functional import multiclass_accuracy
+from torch.nn import MSELoss
+from torcheval.metrics.functional import mean_squared_error
 import pii
 from utils.minnie import PermutedMNIST
 from utils.nn import NN
@@ -18,7 +18,7 @@ run = wandb.init(
     entity=pii.ENTITY,
     project="Candidate 1089687's UDL Mini Project 2025",
     config={
-        "name": "Baseline Method (Coreset Only)",
+        "name": "Baseline Method (Coreset Only, Extension)",
         "learning_rate": 0.0001,
         "epochs": 100,
     },
@@ -40,16 +40,16 @@ for i in range(4):
     # Train on current dataset plus coreset
     metagen = PermutedMNIST.metagenerator(perms[i], 256, one_hot=True)
     metagen_with_coreset = helper(metagen, coreset)
-    for avg_loss in mle(model, metagen_with_coreset, KLDivLoss(reduction="batchmean"), lr=0.0001, epochs=100):
+    for avg_loss in mle(model, metagen_with_coreset, MSELoss(), lr=0.0001, epochs=100):
         run.log({"avg_loss": avg_loss})
     # Save weights from first task for reuse by VCL later
     if i == 0:
-        torch.save(model.state_dict(), "mle.pt")
+        torch.save(model.state_dict(), "mle_extension.pt")
     # Log average accuracy
     data = torch.cat(tuple(PermutedMNIST.test_data(perms[j]) for j in range(i+1)))
-    labels = torch.cat(tuple(PermutedMNIST.test_labels() for j in range(i+1)))
+    y = torch.cat(tuple(PermutedMNIST.one_hot_test_labels() for j in range(i+1)))
     preds = model(data)
-    run.log({"acc": multiclass_accuracy(preds, labels)})
+    run.log({"mse": mean_squared_error(preds, y)})
     # Add a random batch of the current dataset to the coreset
     coreset.append(next(metagen()))
 run.finish()
